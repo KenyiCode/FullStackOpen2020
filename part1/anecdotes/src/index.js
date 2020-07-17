8eff27ae165d111eadbd3b38681a5360cf0d38a2
Kenyiro Anazagasty <kenyicode@gmail.com>
2020-07-16 22:36:54 -0400

    Removed unnecessary files and completed exercise 1.12

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+const App = (props) => {
+    const [random, setRandom] = useState(0)
+    
+    const handleClick = () => {
+        setRandom(Math.floor(Math.random()*5))
+    }
+
+    const selected = random
+
+    return (
+        <div>
+            <h1>{props.anecdotes[selected]}</h1> 
+            <button onClick={handleClick}>Randomize</button>
+        </div>
+    )
+}
+
+const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time... The remaining 10 percent accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Debugging is twice as hard as writing code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+
+ReactDOM.render(<App anecdotes={anecdotes}/>, document.getElementById('root'));
