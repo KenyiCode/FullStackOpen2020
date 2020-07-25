0bd423db0c49f961a143eff050c7c31658158359
Kenyiro Anazagasty <kenyicode@gmail.com>
2020-07-25 01:39:39 -0400

    Completed exercise 1.14

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -6,8 +6,11 @@ import ReactDOM from 'react-dom';
 const App = (props) => {
     const [random, setRandom] = useState(0)
     const [votes, setVotes] = useState([0, 0, 0, 0, 0])
+    const [mostVotes, setMostVotes] = useState(0)
     
     let selected = 0
+    let voted = 0
+
     const handleRandClick = () => {
         let tmp = 0
         do {
@@ -20,17 +23,32 @@ const App = (props) => {
     selected = random
 
     const handleVoteClick = () => {
-        const copy = {...votes}
+        const copy = [...votes]
         copy[selected] += 1
         setVotes(copy)
+
+        let most = 0
+        for (let i = 0; i < copy.length; i++){
+            if (copy[i] > most)
+            {
+                most = copy[i]
+                setMostVotes(i)
+            }
+        }
     }
+
+    voted = mostVotes
 
     return (
         <div>
-            <h1>{props.anecdotes[selected]}</h1> 
+            <h1>Anecdote of the day</h1>
+            <p>{props.anecdotes[selected]}</p> 
             <p>Has {votes[selected]} votes</p>
-            <button onClick={handleRandClick}>Randomize</button>
             <button onClick={handleVoteClick}>Vote</button>
+            <button onClick={handleRandClick}>Next Anecdote</button>
+            <h1>Anecdote with most votes</h1>
+            <p>{props.anecdotes[voted]}</p>
+            <p>Has {votes[voted]} votes</p>
         </div>
     )
 }
